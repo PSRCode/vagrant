6b6d367a8a9cc4ce0433941a0e6f7df4e7247a34
Jonathan Rajotte <jonathan.rajotte-julien@efficios.com>
2019-07-05 17:42:56 -0400

    Fix: use $1 for first argument
    Signed-off-by: Jonathan Rajotte <jonathan.rajotte-julien@efficios.com>

--- blog/Debugging Python applications on Linux with LTTng-UST/py_tracing_syscalls.sh
+++ blog/Debugging Python applications on Linux with LTTng-UST/py_tracing_syscalls.sh
@@ -1,6 +1,6 @@
 lttng create py_tracing_syscalls --live
 lttng enable-event --python --all
 lttng enable-event --kernel --syscall --all
-lttng track --kernel --pid "$0"
+lttng track --kernel --pid "$1"
 lttng start
 lttng view
